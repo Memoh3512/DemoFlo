8c7bb3a0345f7c561cda693f915d95f0e1bcab21
Memoh <CraftMan3512@gmail.com>
2022-01-02 15:12:41 -0500

    play sfx script

---
+++ Assets/PlaySFX.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySFX : MonoBehaviour
+{
+
+    public void PlayButtonSFX()
+    {
+        
+        //TODO FLO son bouton menu
+        
+        
+    }
+    
+}
